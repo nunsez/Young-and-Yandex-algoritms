3ae71b62bfdfbb7feda148da0a809dad994fad9d
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-06-25 12:08:12 +0700

    add 4E

--- lesson-4/4E.js
+++ lesson-4/4E.js
@@ -1,7 +1,18 @@
 const fs = require('fs');
 
-const solution = () => {
+const solution = ([n, ...rest]) => {
+    const len = Number(n);
+    const max = {};
 
+    for (let i = 0; i < len; i += 1) {
+        const [w, h] = rest[i].split(' ').map(Number);
+
+        max[w] = Math.max(max[w] ?? 0, h)
+    }
+
+    const ans = Object.values(max).reduce((acc, v) => acc + v, 0);
+
+    return ans.toString();
 };
 
 const input = fs.readFileSync('input.txt', 'utf-8');
